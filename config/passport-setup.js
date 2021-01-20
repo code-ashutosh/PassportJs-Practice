0f828904bc5fdd3652fc5976183548bd9ccfe791
code-ashutosh <in.ashutoshtrivedi@gmail.com>
2021-01-21 01:43:54 +0530

    completes tutorial 6 & 7

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,9 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 
+//import keys
+const keys = require('./keys');
+
 passport.use(
     new GoogleStrategy({
         // options for the google auth
+        clientID: keys.google.clientID,
+        clientSecret:keys.google.clientSecret
     }),
     ()=>{
         // passport callback after the user is being authenticated
