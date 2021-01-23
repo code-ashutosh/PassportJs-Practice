bb1d5943770e3d3e88c5b228b23a0004b5257346
code-ashutosh <in.ashutoshtrivedi@gmail.com>
2021-01-23 16:51:31 +0530

    mongodb connected succesfuly tutorial 10

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 // setting view engine
 app.set('view engine','ejs');
+
+// connect to mongodb
+mongoose.connect(keys.mongodb.uri, {useNewUrlParser:true, useUnifiedTopology:true}, ()=>{
+    // connection established
+    console.log('connected successfully');
+});
 
 // set up auth routes
 app.use('/auth', authRoutes);
